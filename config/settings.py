0cb01174bb88762d7490aa33a78afb206e53f840
Sanzharmoldobekov <sanchomirbecov@gmail.com>
2022-07-01 12:44:45 +0600

    san

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES ['default'].update(db_from_env)
